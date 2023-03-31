b0b1bc1f7e08add4e1a21c8ac9a1ba40f6626019
Fawwaz129 <wazwazzsrhrr@gmail.com>
2023-03-31 10:53:56 +0700

    hayu

--- web/app/Http/Controllers/TaskController.php
+++ web/app/Http/Controllers/TaskController.php
@@ -14,7 +14,7 @@ use App\Models\Userrole;
 class TaskController extends Controller
 {
     public function __construct(){
-        //$this->middleware('auth');
+        $this->middleware('auth');
         //$this->middleware('is_admin');
     }
     public function index(Request $request) {
